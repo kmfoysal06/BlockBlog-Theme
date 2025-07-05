976fe449d11c0766d278b92dbbe9d918a7676e85
kmfoysal06 <foysalkazimd01@gmail.com>
2025-07-05 15:04:25 +0600

    added PrismJS styles and scripts for code highlighting on single pages

--- inc/classes/assets.php
+++ inc/classes/assets.php
@@ -23,13 +23,22 @@ class Assets
     public function load_scripts() {
         $css_path = '/assets/css/';
         $js_path = '/assets/js/';
+        $prisma_path = '/assets/prism/';
         wp_register_style("blockblog_main", BLOCKBLOG_URI . $css_path . 'main.css', [], filemtime(BLOCKBLOG_DIR . $css_path . 'main.css'));
         wp_register_style("blockblog_utilities", BLOCKBLOG_URI . $css_path . 'utilities.css', [], filemtime(BLOCKBLOG_DIR . $css_path . 'utilities.css'));
+        wp_register_style("blockblog_prism_css", BLOCKBLOG_URI . $prisma_path . 'prism.css', [], filemtime(BLOCKBLOG_DIR . $prisma_path . 'prism.css'));
+        
+        wp_register_script("blockblog_prism_js", BLOCKBLOG_URI . $prisma_path . 'prism.js', [], filemtime(BLOCKBLOG_DIR . $prisma_path . 'prism.js'), true);
         wp_register_script("blockblog_main_js", BLOCKBLOG_URI . $js_path . 'main.js', [], filemtime(BLOCKBLOG_DIR . $js_path . 'main.js'), true);
     
         wp_enqueue_style("blockblog_main");
         wp_enqueue_style("blockblog_utilities");
         wp_enqueue_style("dashicons");
         wp_enqueue_script("blockblog_main_js");
+
+        if(is_single() || is_page()) {
+            wp_enqueue_style("blockblog_prism_css");
+            wp_enqueue_script("blockblog_prism_js");
+        }
     }
 }
