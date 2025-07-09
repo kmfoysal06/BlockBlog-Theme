f283a5b06940faef2c9837a475d81c68ba7fa4e0
kmfoysal06 <foysalkazimd01@gmail.com>
2025-07-09 11:43:14 +0600

    Add .gitignore and update asset paths in assets.php for improved script loading

--- inc/classes/assets.php
+++ inc/classes/assets.php
@@ -21,18 +21,15 @@ class Assets
         add_action('wp_enqueue_scripts', [$this, 'load_scripts']);
     }
     public function load_scripts() {
-        $css_path = '/assets/css/';
-        $js_path = '/assets/js/';
-        $prisma_path = '/assets/prism/';
+        $css_path = '/assets/dist/css/';
+        $js_path = '/assets/dist/js/';
         wp_register_style("blockblog_main", BLOCKBLOG_URI . $css_path . 'main.css', [], filemtime(BLOCKBLOG_DIR . $css_path . 'main.css'));
-        wp_register_style("blockblog_utilities", BLOCKBLOG_URI . $css_path . 'utilities.css', [], filemtime(BLOCKBLOG_DIR . $css_path . 'utilities.css'));
-        wp_register_style("blockblog_prism_css", BLOCKBLOG_URI . $prisma_path . 'prism.css', [], filemtime(BLOCKBLOG_DIR . $prisma_path . 'prism.css'));
+        wp_register_style("blockblog_prism_css", BLOCKBLOG_URI . $css_path . 'prism.css', [], filemtime(BLOCKBLOG_DIR . $css_path . 'prism.css'));
         
-        wp_register_script("blockblog_prism_js", BLOCKBLOG_URI . $prisma_path . 'prism.js', [], filemtime(BLOCKBLOG_DIR . $prisma_path . 'prism.js'), true);
+        wp_register_script("blockblog_prism_js", BLOCKBLOG_URI . $js_path . 'prism.js', [], filemtime(BLOCKBLOG_DIR . $js_path . 'prism.js'), true);
         wp_register_script("blockblog_main_js", BLOCKBLOG_URI . $js_path . 'main.js', [], filemtime(BLOCKBLOG_DIR . $js_path . 'main.js'), true);
     
         wp_enqueue_style("blockblog_main");
-        wp_enqueue_style("blockblog_utilities");
         wp_enqueue_style("dashicons");
         wp_enqueue_script("blockblog_main_js");
 
